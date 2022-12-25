4b93cc4152a8fd295257525b45a9f5f4d17ef99c
EAOzz <katrinkaya86@gmail.com>
2022-12-25 14:44:10 +0300

    Home work 15.3

--- HW_14/cypress/e2e/test-santa/HW_15_2.cy.js
+++ HW_14/cypress/e2e/test-santa/HW_15_2.cy.js
@@ -1,37 +1,35 @@
-describe('example to-do app', () => {
+describe('click links', () => {
   beforeEach(() => {
     cy.request({
       method: 'POST',
       url: 'api/login?redirect=%2',
       body: {
-        email: 'katarinami@inbox.ru',
-        password: '{{$passwordNew}}',
+        email: Cypress.env('username'),
+        password: Cypress.env('password'),
              }
     })
-      // Переходим на главную страницу приложения
-    cy.visit('')
-    
+    cy.visit('') // Переходим на главную страницу приложения
   })
 
-  it('click links', () => {
+  it('click links new box', () => {
     cy.contains('Создать коробку').click(),
     cy.location('pathname').should('eq', '/box/new'),
     cy.get('.form-card__header').first().should('have.text', 'Придумайте название коробке')
   })
 
-   it('click links', () => {
+   it('click links randomizer', () => {
     cy.contains('Быстрая жеребьевка').click(),
     cy.location('pathname').should('eq', '/randomizer'),
     cy.get('.form-card__header').first().should('have.text', 'Быстрая жеребьевка')
   })
 
-  it('click links', () => {
+  it('click links boxes', () => {
     cy.get('a[href*="/account/boxes"]').last().click(),
     cy.location('pathname').should('eq', '/account/boxes'),
     cy.get('.footer-item').first().should('have.text', 'Быстрая жеребьевка')
   })
 
-  it('click links', () => {
+  it('click links account', () => {
     cy.get('a[href*="/account"]').last().click(),
     cy.location('pathname').should('eq', '/account'),
     cy.get('[id="account_profile_settings"]').should('have.text', 'Настройки профиля')
